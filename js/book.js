b4c0f6a81d40b1aee82f5cea1c9d012a31eb5139
nowyouhere <131799031+nowyouhere@users.noreply.github.com>
2023-05-01 09:09:53 +0600

    Add files via upload

---
+++ js/book.js
@@ -0,0 +1,93 @@
+const pages = document.querySelectorAll('.page');
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+const pagesCount = document.querySelector('.pages-count');
+const currentPageNumber = document.querySelector('.current-page-number');
+const chandeModeBtn = document.querySelector('#change-mode-btn');
+
+
+prevBtn.addEventListener('click', prevPage);
+nextBtn.addEventListener('click', nextPage);
+chandeModeBtn.addEventListener('click', changeMode);
+
+let currentPage = 0;
+let touchStartX = null;
+let touchEndX = null;
+let minSwipeDistance = 150;
+let isOn = false;
+
+function prevPage() {
+    pages[currentPage].classList.remove('active-page');
+    currentPage = (currentPage - 1 + pages.length) % pages.length;
+    pages[currentPage].classList.add('active-page');
+    currentPageNumber.innerHTML = currentPage + 1;
+    pages[currentPage].scrollIntoView({ behavior: 'smooth' });
+    if (!isOn) {
+        pages[currentPage].classList.add('page-dark-mode');
+    }
+}
+
+function nextPage() {
+    pages[currentPage].classList.remove('active-page');
+    currentPage = (currentPage + 1) % pages.length;
+    pages[currentPage].classList.add('active-page');
+    currentPageNumber.innerHTML = currentPage + 1;
+    pages[currentPage].scrollIntoView({ behavior: 'smooth' });
+}
+
+pagesCount.innerHTML = pages.length;
+
+function handleTouchStart(event) {
+    touchStartX = event.changedTouches[0].clientX;
+}
+
+function handleTouchMove(event) {
+    event.preventDefault();
+    touchEndX = event.changedTouches[0].clientX;
+}
+
+function handleTouchEnd() {
+    if (touchEndX < touchStartX - minSwipeDistance) {
+      nextPage();
+    } else if (touchEndX > touchStartX + minSwipeDistance) {
+      prevPage();
+    }
+    touchStartX = null;
+    touchEndX = null;
+}
+
+
+const book = document.querySelector('.book');
+book.addEventListener('touchstart', handleTouchStart);
+book.addEventListener('touchmove', handleTouchMove);
+book.addEventListener('touchend', handleTouchEnd);
+
+function changeMode() {
+    const body = document.body;
+    const main = document.querySelector('.main');
+    const buttons = document.querySelectorAll('.btn');
+    if (isOn) {
+        chandeModeBtn.classList.remove('change-mode-btn_dark');
+        body.classList.remove('body-dark-mode');
+        main.classList.remove('main-dark-mode');
+        buttons.forEach(btn => {
+            btn.classList.remove('btn-dark-mode');
+        });
+        pages.forEach(page => {
+            page.classList.remove('page-dark-mode');
+        });
+    }
+    else {
+        chandeModeBtn.classList.add('change-mode-btn_dark');
+        body.classList.add('body-dark-mode');
+        main.classList.add('main-dark-mode');
+        buttons.forEach(btn => {
+            btn.classList.add('btn-dark-mode');
+        });
+        pages.forEach(page => {
+            page.classList.add('page-dark-mode');
+        });
+    }
+
+    isOn = !isOn;
+}
---
+++ js/main.js
@@ -0,0 +1,52 @@
+function openService(evt, serviceName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(serviceName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+const slider = document.querySelector('.slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});
