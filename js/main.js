28d3a124a2ee050e0fa2693b90f1fcdf5fad8568
nowyouhere <131799031+nowyouhere@users.noreply.github.com>
2023-05-04 04:00:56 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,34 +19,6 @@ function openService(evt, serviceName) {
   evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
-
-const slider = document.querySelector('.slider');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', e => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-
-slider.addEventListener('mousemove', e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3;
-  slider.scrollLeft = scrollLeft - walk;
-});
+const activeTab = document.getElementById("defaultOpen");
+activeTab.click();
+activeTab.style.order = -1;
